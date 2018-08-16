b0f053c0184c903a33a7862595b1f0cf463dcf69
Himani Shroff <himani.shroff@Yash.com>
2018-08-16 18:32:03 +0530

    change in sequence of getting todos

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ var app = express();
 app.use(bodyParser.json());
 
 
+
+app.get('/todos', (req,res)=>{
+    console.log('inside todos');
+    Todo.find().then((docs) => {
+        console.log('inside success');
+        res.send(docs);
+    },(err)=>{
+        res.status(400).send(err);
+    })
+})
+
 app.get('/todos/:id', (req,res)=>{
     var id = req.params.id;
     console.log(res);
@@ -48,15 +59,6 @@ app.get('/todos/:id', (req,res)=>{
 //     })
 // })
 
-app.get('/todos', (req,res)=>{
-    console.log('inside todos');
-    Todo.find().then((docs) => {
-        console.log('inside success');
-        res.send(docs);
-    },(err)=>{
-        res.status(400).send(err);
-    })
-})
 
 
 
