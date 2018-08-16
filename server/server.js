97c3766f6c08fd2593879c9f6db40ff10be8494c
Himani Shroff <himani.shroff@Yash.com>
2018-08-16 13:56:48 +0530

    mongoose changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -9,21 +10,69 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-    console.log(`body: ${JSON.stringify(req.body)}`)
-    var todo = new Todo({
-        text: req.body.text,
-        completed: req.body.completed
-    })
 
-    todo.save().then((doc)=>{
-        res.send(doc);
-        console.log(doc);
-    },(err)=>{
-        res.status(400).send(err);
+app.get('/todos/:id', (req,res)=>{
+    var id = req.params.id;
+    console.log(res);
+    if(!ObjectID.isValid(id)){
+       return res.status(400).send("Invalid ID")
+    }
+    Todo.findById(id).then((todo)=>{
+        if(todo){
+            res.send(todo);
+        }else{
+            res.status(404).send("No data found")
+        }
+        
+    }).catch((e)=>{
+        res.status(400).send(e);
     })
 })
+
+
+
+// app.post('/todos',(req,res)=>{
+//     console.log(`body: ${JSON.stringify(req.body)}`)
+//     var todo = new Todo({
+//         text: req.body.text,
+//         completed: req.body.completed
+//     })
+
+//     todo.save().then((doc)=>{
+//         res.send(doc);
+//         console.log(doc);
+//     },(err)=>{
+//         res.status(400).send(err);
+//     })
+// })
+
+// app.get('/todos', (req,res)=>{
+//     Todo.find({text: 'Cook Dinner'}).then((docs) => {
+//         res.send(docs);
+//     },(err)=>{
+//         res.status(400).send(err);
+//     })
+// })
+
+
+
+/* mongoose queries */
+
+// User.findById('5b718a08b0607c1410f5a106').then((doc)=>{
+//     if(!doc){
+//         return console.log("No data found");
+//     }
+//     console.log(`Data: ${doc}`);
+// }).catch((e)=>{
+//     console.log('Error while execution');
+// })
 
 app.listen(3000, ()=>{
     console.log('Starting on port 3000');
 })
+
+
+module.exports={app};
+
+
+
